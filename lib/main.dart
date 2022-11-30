8b4683874a3abc033fa062797e1521aeeec64570
AnnaChrzaszcz <achrzaszcz13@gmail.com>
2022-11-30 12:00:58 +0100

    add global button style

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:clean_mates_app/screens/add_new_room.dart';
+
 import './screens/dashbord_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -19,7 +21,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color.fromRGBO(167, 34, 110, 1),
+        primarySwatch: Colors.pink,
+        appBarTheme: AppBarTheme(color: Color.fromRGBO(167, 34, 110, 1)),
+        iconTheme: IconThemeData(color: Color.fromRGBO(47, 149, 153, 1)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                Color.fromRGBO(47, 149, 153, 1),
+              ),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: BorderSide.none))),
+        ),
+        textButtonTheme: TextButtonThemeData(
+            style: ButtonStyle(
+          foregroundColor:
+              MaterialStateProperty.all(Color.fromRGBO(47, 149, 153, 1)),
+        )),
+        //Color.fromRGBO(247, 219, 79, 1)
+        accentColor: Color.fromRGBO(47, 149, 153, 1),
       ),
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
@@ -30,7 +52,10 @@ class MyApp extends StatelessWidget {
           return AuthScreen();
         },
       ),
-      routes: {DashboardScreen.routeName: (context) => DashboardScreen()},
+      routes: {
+        DashboardScreen.routeName: (context) => DashboardScreen(),
+        AddNewRoom.routeName: (context) => AddNewRoom(),
+      },
     );
   }
 }
