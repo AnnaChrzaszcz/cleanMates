5138b570ca02c9be0e662ae6e1d6cc5ec7f31dd0
AnnaChrzaszcz <achrzaszcz13@gmail.com>
2022-12-10 20:19:56 +0100

    custom exception

---
+++ lib/models/exceptions/logistic_expection.dart
@@ -0,0 +1,10 @@
+class LogisticExpection implements Exception {
+  final String message;
+  LogisticExpection(this.message);
+
+  @override
+  String toString() {
+    return message;
+    //return super.toString();
+  }
+}
