cfb31633d9a66ac1c808b552171e740de1fbbbcf
AnnaChrzaszcz <achrzaszcz13@gmail.com>
2023-01-03 14:33:05 +0100

    responsive save activity container

--- lib/widgets/activity/save_activity_container.dart
+++ lib/widgets/activity/save_activity_container.dart
@@ -72,79 +72,85 @@ class _SaveActivityContainerState extends State<SaveActivityContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 10,
-        ),
-        CircleAvatar(
-          radius: 30,
-          backgroundColor: Theme.of(context).primaryColor,
-          foregroundColor: Colors.white,
-          child: Text(
-            '${activitesPointsSum}',
-            style: TextStyle(fontSize: 15),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10,
           ),
-        ),
-        Container(
-          height: 600,
-          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-          //decoration: BoxDecoration(border: Border.all(color: Colors.pink)),
-          child: ListView.builder(
-            itemCount: widget.activities.length,
-            itemBuilder: ((context, index) => Column(
-                  children: [
-                    CheckboxListTile(
-                      title: Text(
-                        widget.activities[index].activityName,
-                        style: TextStyle(),
-                      ),
-                      secondary: CircleAvatar(
-                        radius: 20,
-                        backgroundColor: selectedIndexes.contains(index)
-                            ? Theme.of(context).primaryColor
-                            : Theme.of(context).dividerColor,
-                        foregroundColor: Colors.white,
-                        child: Text('${widget.activities[index].points}'),
-                      ),
-                      value: selectedIndexes.contains(index),
-                      onChanged: (_) {
-                        if (selectedIndexes.contains(index)) {
-                          setState(() {
-                            selectedIndexes.remove(index);
-                            activitesPointsSum -=
-                                widget.activities[index].points;
-                          });
-                        } else {
-                          setState(() {
-                            selectedIndexes.add(index);
-                            activitesPointsSum +=
-                                widget.activities[index].points;
-                          });
-                        }
-                      },
-                      activeColor: Theme.of(context).primaryColor,
-                    ),
-                    Divider()
-                  ],
-                )),
-          ),
-        ),
-        ElevatedButton(
-          onPressed:
-              selectedIndexes.length <= 0 ? null : () => _saveActivites(),
-          child: _isLoading ? CircularProgressIndicator() : Text('Save'),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith<Color>(
-              (Set<MaterialState> states) {
-                if (states.contains(MaterialState.disabled)) return Colors.grey;
-                return Theme.of(context)
-                    .primaryColor; // Use the component's default.
-              },
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Colors.white,
+            child: Text(
+              '${activitesPointsSum}',
+              style: TextStyle(fontSize: 15),
             ),
           ),
-        )
-      ],
+          Expanded(
+            child: Container(
+              height: 600,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+              //decoration: BoxDecoration(border: Border.all(color: Colors.pink)),
+              child: ListView.builder(
+                itemCount: widget.activities.length,
+                itemBuilder: ((context, index) => Column(
+                      children: [
+                        CheckboxListTile(
+                          title: Text(
+                            widget.activities[index].activityName,
+                            style: TextStyle(),
+                          ),
+                          secondary: CircleAvatar(
+                            radius: 20,
+                            backgroundColor: selectedIndexes.contains(index)
+                                ? Theme.of(context).primaryColor
+                                : Theme.of(context).dividerColor,
+                            foregroundColor: Colors.white,
+                            child: Text('${widget.activities[index].points}'),
+                          ),
+                          value: selectedIndexes.contains(index),
+                          onChanged: (_) {
+                            if (selectedIndexes.contains(index)) {
+                              setState(() {
+                                selectedIndexes.remove(index);
+                                activitesPointsSum -=
+                                    widget.activities[index].points;
+                              });
+                            } else {
+                              setState(() {
+                                selectedIndexes.add(index);
+                                activitesPointsSum +=
+                                    widget.activities[index].points;
+                              });
+                            }
+                          },
+                          activeColor: Theme.of(context).primaryColor,
+                        ),
+                        Divider()
+                      ],
+                    )),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed:
+                selectedIndexes.length <= 0 ? null : () => _saveActivites(),
+            child: _isLoading ? CircularProgressIndicator() : Text('Save'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.disabled))
+                    return Colors.grey;
+                  return Theme.of(context)
+                      .primaryColor; // Use the component's default.
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
