c02173f00002b3d1fab9a513d3b5aacf16886ad4
AnnaChrzaszcz <achrzaszcz13@gmail.com>
2023-01-04 16:24:10 +0100

    spinner waiting for image to load

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -1,6 +1,7 @@
 import 'dart:io'; // at beginning of file
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class UserImagePicker extends StatefulWidget {
   UserImagePicker(this.imagePickFn);
@@ -10,16 +11,51 @@ class UserImagePicker extends StatefulWidget {
   _UserImagePickerState createState() => _UserImagePickerState();
 }
 
-class _UserImagePickerState extends State<UserImagePicker> {
+class _UserImagePickerState extends State<UserImagePicker>
+    with SingleTickerProviderStateMixin {
   File pickedImageFile;
+  bool isLoading = false;
+  AnimationController _animationController;
+  Animation<double> _imageAnimation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    _imageAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _pickImage() async {
     final picker = ImagePicker();
+    setState(() {
+      isLoading = true;
+    });
     final pickedImage = await picker.pickImage(
       source: ImageSource.gallery,
       imageQuality: 1,
     ); //maxWidth: 150
+
+    if (pickedImage != null) {
+      setState(() {
+        pickedImageFile = File(pickedImage.path);
+        isLoading = false;
+      });
+    }
+
     setState(() {
-      pickedImageFile = File(pickedImage.path);
+      isLoading = false;
     });
     widget.imagePickFn(pickedImageFile);
   }
@@ -28,12 +64,26 @@ class _UserImagePickerState extends State<UserImagePicker> {
   Widget build(BuildContext context) {
     return Column(children: [
       Expanded(
-        child: CircleAvatar(
-          radius: 40,
-          backgroundColor: Colors.grey,
-          backgroundImage:
-              pickedImageFile != null ? FileImage(pickedImageFile) : null,
-        ),
+        child: !isLoading
+            ? pickedImageFile != null
+                ? CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: Image(
+                      image: FileImage(pickedImageFile),
+                      opacity: _imageAnimation,
+                    ).image,
+                  )
+                : const CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.grey,
+                  )
+            : CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.grey,
+                child: LoadingAnimationWidget.hexagonDots(
+                    color: Colors.white, size: 20),
+              ),
       ),
       Expanded(
         child: TextButton.icon(
