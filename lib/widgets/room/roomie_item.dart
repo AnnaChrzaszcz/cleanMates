920e92d498fab99ac98a200e56ff7d16c84e3bde
AnnaChrzaszcz <achrzaszcz13@gmail.com>
2023-01-09 15:37:50 +0100

    room your profile picture update

--- lib/widgets/room/roomie_item.dart
+++ lib/widgets/room/roomie_item.dart
@@ -21,85 +21,59 @@ class RoomieItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-        elevation: 30,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-          child: Column(children: [
-            Row(
-              children: [
-                Expanded(
+    if (FirebaseAuth.instance.currentUser.uid ==
+        FirebaseAuth.instance.currentUser.photoURL) {
+      print(name);
+    }
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+      elevation: 30,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        child: Column(children: [
+          Row(
+            children: [
+              Expanded(
+                child: CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Color.fromRGBO(247, 219, 79, 1),
                   child: CircleAvatar(
-                    radius: 45,
-                    backgroundColor: Color.fromRGBO(247, 219, 79, 1),
-                    child: CircleAvatar(
-                        radius: 42,
-                        backgroundImage: Image.network(
-                          imageUrl,
-                          fit: BoxFit.cover,
-                        ).image),
-                  ),
+                      radius: 42,
+                      backgroundImage: NetworkImage(
+                          userId == FirebaseAuth.instance.currentUser.uid
+                              ? FirebaseAuth.instance.currentUser.photoURL
+                              : imageUrl)),
                 ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        name,
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Text(
-                        '${points.toString()} points',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline6
-                            .copyWith(color: Theme.of(context).primaryColor),
-                      ),
-                    ],
-                  ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Text(
+                      '${points.toString()} points',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(color: Theme.of(context).primaryColor),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            ElevatedButton(
-                onPressed: () => selectUser(context),
-                child: Text('Go to profile'))
-          ]),
-        ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          ElevatedButton(
+              onPressed: () => selectUser(context),
+              child: Text('Go to profile'))
+        ]),
       ),
-      // child: GridTile(
-      //   child: GestureDetector(
-      //     onTap: () {
-      //       selectUser(context);
-      //     },
-      //     child: Image.network(
-      //       imageUrl,
-      //       fit: BoxFit.cover,
-      //     ),
-      //   ),
-      //   footer: Container(
-      //     padding: EdgeInsets.all(10),
-      //     color: Colors.black54,
-      //     child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      //       Text(
-      //         name,
-      //         style: TextStyle(color: Colors.white),
-      //       ),
-      //       SizedBox(
-      //         width: 5,
-      //       ),
-      //       Text(
-      //         '${points.toString()} points',
-      //         style: TextStyle(color: Colors.white),
-      //       )
-      //     ]),
-      //   ),
-      // ),
     );
   }
 }
