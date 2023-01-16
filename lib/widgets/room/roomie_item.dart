848f3a777e8d1f3e19e444e2910ebb0818d892b8
AnnaChrzaszcz <achrzaszcz13@gmail.com>
2023-01-16 23:28:37 +0100

    gesture detector on roomie item

--- lib/widgets/room/roomie_item.dart
+++ lib/widgets/room/roomie_item.dart
@@ -11,64 +11,63 @@ class RoomieItem extends StatelessWidget {
 
   RoomieItem(this.userId, this.name, this.imageUrl, this.points);
 
-  void selectUser(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed(UserDashboardScreen.routeName, arguments: {
-      'userId': userId,
-      'name': name,
-      'imageUrl': imageUrl,
-      'points': points
-    });
+  void _selectUser(BuildContext ctx) {
+    Navigator.of(ctx).pushReplacementNamed(UserDashboardScreen.routeName);
   }
 
   @override
   Widget build(BuildContext context) {
     User user = FirebaseAuth.instance.currentUser;
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-      elevation: 30,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        child: Column(children: [
-          Row(
-            children: [
-              Expanded(
-                child: CircleAvatar(
-                  radius: 45,
-                  backgroundColor: Color.fromRGBO(247, 219, 79, 1),
+    return GestureDetector(
+      onTap: name == user.displayName ? () => _selectUser(context) : () {},
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        elevation: 30,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(children: [
+            Row(
+              children: [
+                Expanded(
                   child: CircleAvatar(
-                      radius: 42, backgroundImage: NetworkImage(imageUrl)),
+                    radius: 45,
+                    backgroundColor: Color.fromRGBO(247, 219, 79, 1),
+                    child: CircleAvatar(
+                        radius: 42, backgroundImage: NetworkImage(imageUrl)),
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      name,
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                    Text(
-                      '${points.toString()} points',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline6
-                          .copyWith(color: Theme.of(context).primaryColor),
-                    ),
-                  ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      Text(
+                        '${points.toString()} points',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            .copyWith(color: Theme.of(context).primaryColor),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          if (name != user.displayName)
-            ElevatedButton(
-                onPressed: () => Navigator.of(context)
-                    .pushNamed(SaveActivityScreen.routeName, arguments: userId),
-                child: Text('Save activity as ${name}'))
-        ]),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            if (name != user.displayName)
+              ElevatedButton(
+                  onPressed: () => Navigator.of(context).pushNamed(
+                      SaveActivityScreen.routeName,
+                      arguments: userId),
+                  child: Text('Save activity as ${name}'))
+          ]),
+        ),
       ),
     );
   }
