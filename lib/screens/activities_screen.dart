d581d2355e763dccf7c802b52d316885b301b741
AnnaChrzaszcz <achrzaszcz13@gmail.com>
2023-01-17 13:09:25 +0100

    no activites/gifts screen

--- lib/screens/activities_screen.dart
+++ lib/screens/activities_screen.dart
@@ -1,8 +1,10 @@
+import 'dart:math' as Math;
 import 'package:clean_mates_app/models/room.dart';
 import 'package:clean_mates_app/screens/edit_activity_screen.dart';
 import 'package:clean_mates_app/widgets/app_drawer.dart';
 import 'package:clean_mates_app/widgets/fab/custom_add_fab.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:rive/rive.dart';
 import '../providers/activities_provider.dart';
@@ -80,83 +82,120 @@ class ActivitiesScreen extends StatelessWidget {
                 ),
                 onRefresh: () => _refreshActivities(context, myRoom.id),
                 child: Consumer<ActivitiesProvider>(
-                  builder: ((ctx, activitiesData, _) => Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                        child: ListView.builder(
-                          itemCount: activitiesData.activities.length,
-                          itemBuilder: ((context, index) => Column(
-                                children: [
-                                  ListTile(
-                                    trailing: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        IconButton(
-                                          icon: Icon(Icons.edit),
-                                          onPressed: () {
-                                            Navigator.of(context).pushNamed(
-                                                EditActivityScreen.routeName,
-                                                arguments: {
-                                                  'id': activitiesData
-                                                      .activities[index].id,
-                                                });
-                                          },
-                                        ),
-                                        IconButton(
-                                            icon: Icon(Icons.delete),
+                  builder: ((ctx, activitiesData, _) => activitiesData
+                          .activities.isEmpty
+                      ? Container(
+                          padding: EdgeInsets.all(8.0),
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                'You have no activities yet',
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Text(
+                                'Define some activities below (+)',
+                                style: Theme.of(context).textTheme.headline6,
+                              ),
+                              RotatedBox(
+                                  quarterTurns: 2,
+                                  child: Container(
+                                    width: double.infinity * 1 / 3,
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 40, horizontal: 10),
+                                    child: Lottie.asset(
+                                      'assets/animations/lottie/box.json',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        )
+                      : Container(
+                          margin:
+                              EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                          child: ListView.builder(
+                            itemCount: activitiesData.activities.length,
+                            itemBuilder: ((context, index) => Column(
+                                  children: [
+                                    ListTile(
+                                      trailing: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          IconButton(
+                                            icon: Icon(Icons.edit),
                                             onPressed: () {
-                                              showDialog(
-                                                context: context,
-                                                builder: (ctx) => AlertDialog(
-                                                  title: Text('Are you sure?'),
-                                                  content: Text(
-                                                      'Do you want to remove the item from the cart?'),
-                                                  actions: [
-                                                    TextButton(
-                                                        onPressed: () {
-                                                          Navigator.of(ctx)
-                                                              .pop();
-                                                        },
-                                                        child: Text('NO')),
-                                                    TextButton(
-                                                        onPressed: () {
-                                                          Navigator.of(ctx)
-                                                              .pop();
-                                                          _deleteActivity(
-                                                              context,
-                                                              activitiesData
-                                                                  .activities[
-                                                                      index]
-                                                                  .id);
-                                                        },
-                                                        child: Text('YES')),
-                                                  ],
-                                                ),
-                                              );
-                                            }),
-                                      ],
+                                              Navigator.of(context).pushNamed(
+                                                  EditActivityScreen.routeName,
+                                                  arguments: {
+                                                    'id': activitiesData
+                                                        .activities[index].id,
+                                                  });
+                                            },
+                                          ),
+                                          IconButton(
+                                              icon: Icon(Icons.delete),
+                                              onPressed: () {
+                                                showDialog(
+                                                  context: context,
+                                                  builder: (ctx) => AlertDialog(
+                                                    title:
+                                                        Text('Are you sure?'),
+                                                    content: Text(
+                                                        'Do you want to remove this activity?'),
+                                                    actions: [
+                                                      TextButton(
+                                                          onPressed: () {
+                                                            Navigator.of(ctx)
+                                                                .pop();
+                                                          },
+                                                          child: Text('NO')),
+                                                      TextButton(
+                                                          onPressed: () {
+                                                            Navigator.of(ctx)
+                                                                .pop();
+                                                            _deleteActivity(
+                                                                context,
+                                                                activitiesData
+                                                                    .activities[
+                                                                        index]
+                                                                    .id);
+                                                          },
+                                                          child: Text('YES')),
+                                                    ],
+                                                  ),
+                                                );
+                                              }),
+                                        ],
+                                      ),
+                                      leading: CircleAvatar(
+                                          radius: 25,
+                                          backgroundColor:
+                                              Theme.of(context).dividerColor,
+                                          foregroundColor: Colors.white,
+                                          child: FittedBox(
+                                            child: Text(
+                                                '${activitiesData.activities[index].points}'),
+                                          )),
+                                      title: Text(
+                                        activitiesData
+                                            .activities[index].activityName,
+                                        style: TextStyle(),
+                                      ),
+                                      subtitle: Text(myRoom.roomName),
                                     ),
-                                    leading: CircleAvatar(
-                                        radius: 25,
-                                        backgroundColor:
-                                            Theme.of(context).dividerColor,
-                                        foregroundColor: Colors.white,
-                                        child: FittedBox(
-                                          child: Text(
-                                              '${activitiesData.activities[index].points}'),
-                                        )),
-                                    title: Text(
-                                      activitiesData
-                                          .activities[index].activityName,
-                                      style: TextStyle(),
-                                    ),
-                                    subtitle: Text(myRoom.roomName),
-                                  ),
-                                  Divider()
-                                ],
-                              )),
-                        ),
-                      )),
+                                    Divider()
+                                  ],
+                                )),
+                          ),
+                        )),
                 ),
               )),
       ),
--- lib/screens/gifts_screen.dart
+++ lib/screens/gifts_screen.dart
@@ -5,6 +5,7 @@ import 'package:clean_mates_app/screens/edit_gift_screen.dart';
 import 'package:clean_mates_app/widgets/app_drawer.dart';
 import 'package:clean_mates_app/widgets/fab/custom_add_fab.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:rive/rive.dart';
 import '../providers/rooms_provider.dart';
@@ -80,81 +81,118 @@ class GiftsScreen extends StatelessWidget {
                   },
                 ),
                 child: Consumer<GiftsProvider>(
-                  builder: ((ctx, giftsData, _) => Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                        child: ListView.builder(
-                          itemCount: giftsData.gifts.length,
-                          itemBuilder: ((context, index) => Column(
-                                children: [
-                                  ListTile(
-                                    trailing: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        IconButton(
-                                          icon: Icon(Icons.edit),
-                                          onPressed: () {
-                                            Navigator.of(context).pushNamed(
-                                                EditGiftScreen.routeName,
-                                                arguments: {
-                                                  'id':
-                                                      giftsData.gifts[index].id,
-                                                });
-                                          },
-                                        ),
-                                        IconButton(
-                                            icon: Icon(Icons.delete),
+                  builder: ((ctx, giftsData, _) => giftsData.gifts.isEmpty
+                      ? Container(
+                          padding: EdgeInsets.all(8.0),
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                'You have no gifts yet',
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Text(
+                                'Define some gifts below (+)',
+                                style: Theme.of(context).textTheme.headline6,
+                              ),
+                              RotatedBox(
+                                  quarterTurns: 2,
+                                  child: Container(
+                                    width: double.infinity * 1 / 3,
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 40, horizontal: 10),
+                                    child: Lottie.asset(
+                                      'assets/animations/lottie/box.json',
+                                      fit: BoxFit.contain,
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        )
+                      : Container(
+                          margin:
+                              EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                          child: ListView.builder(
+                            itemCount: giftsData.gifts.length,
+                            itemBuilder: ((context, index) => Column(
+                                  children: [
+                                    ListTile(
+                                      trailing: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          IconButton(
+                                            icon: Icon(Icons.edit),
                                             onPressed: () {
-                                              showDialog(
-                                                context: context,
-                                                builder: (ctx) => AlertDialog(
-                                                  title: Text('Are you sure?'),
-                                                  content: Text(
-                                                      'Do you want to remove the item from the cart?'),
-                                                  actions: [
-                                                    TextButton(
-                                                        onPressed: () {
-                                                          Navigator.of(ctx)
-                                                              .pop();
-                                                        },
-                                                        child: Text('NO')),
-                                                    TextButton(
-                                                        onPressed: () {
-                                                          Navigator.of(ctx)
-                                                              .pop();
-                                                          _deleteGift(
-                                                              context,
-                                                              giftsData
-                                                                  .gifts[index]
-                                                                  .id);
-                                                        },
-                                                        child: Text('YES')),
-                                                  ],
-                                                ),
-                                              );
-                                            }),
-                                      ],
+                                              Navigator.of(context).pushNamed(
+                                                  EditGiftScreen.routeName,
+                                                  arguments: {
+                                                    'id': giftsData
+                                                        .gifts[index].id,
+                                                  });
+                                            },
+                                          ),
+                                          IconButton(
+                                              icon: Icon(Icons.delete),
+                                              onPressed: () {
+                                                showDialog(
+                                                  context: context,
+                                                  builder: (ctx) => AlertDialog(
+                                                    title:
+                                                        Text('Are you sure?'),
+                                                    content: Text(
+                                                        'Do you want to remove this gift?'),
+                                                    actions: [
+                                                      TextButton(
+                                                          onPressed: () {
+                                                            Navigator.of(ctx)
+                                                                .pop();
+                                                          },
+                                                          child: Text('NO')),
+                                                      TextButton(
+                                                          onPressed: () {
+                                                            Navigator.of(ctx)
+                                                                .pop();
+                                                            _deleteGift(
+                                                                context,
+                                                                giftsData
+                                                                    .gifts[
+                                                                        index]
+                                                                    .id);
+                                                          },
+                                                          child: Text('YES')),
+                                                    ],
+                                                  ),
+                                                );
+                                              }),
+                                        ],
+                                      ),
+                                      leading: CircleAvatar(
+                                          radius: 25,
+                                          backgroundColor:
+                                              Theme.of(context).dividerColor,
+                                          foregroundColor: Colors.white,
+                                          child: FittedBox(
+                                            child: Text(
+                                                '${giftsData.gifts[index].points}'),
+                                          )),
+                                      title: Text(
+                                        giftsData.gifts[index].giftName,
+                                        style: TextStyle(),
+                                      ),
+                                      subtitle: Text(myRoom.roomName),
                                     ),
-                                    leading: CircleAvatar(
-                                        radius: 25,
-                                        backgroundColor:
-                                            Theme.of(context).dividerColor,
-                                        foregroundColor: Colors.white,
-                                        child: FittedBox(
-                                          child: Text(
-                                              '${giftsData.gifts[index].points}'),
-                                        )),
-                                    title: Text(
-                                      giftsData.gifts[index].giftName,
-                                      style: TextStyle(),
-                                    ),
-                                    subtitle: Text(myRoom.roomName),
-                                  ),
-                                  Divider()
-                                ],
-                              )),
-                        ),
-                      )),
+                                    Divider()
+                                  ],
+                                )),
+                          ),
+                        )),
                 ),
               )),
       ),
